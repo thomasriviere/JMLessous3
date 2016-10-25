e378bb2ef72fb038072a1a9a5b734b561f4bb97a
thomasriviere <thomasr1122@hotmail.fr>
2016-10-25 18:53:43 +0200

    Update Client.java

--- src/java/JMLessous/entity/Client.java
+++ src/java/JMLessous/entity/Client.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 
-
+/*ddddd
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
